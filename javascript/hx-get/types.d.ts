c84b4275ef5fca82ac5978b51b083d1640f7d638
Wirune Kaewjai <wirune.kae@hotmail.com>
2024-06-01 11:42:32 +0700

    fix exports

--- javascript/hx-get/types.d.ts
+++ javascript/hx-get/types.d.ts
@@ -1,1 +1,2 @@
 export type Callback = (pathname: string, query: URLSearchParams) => Promise<string | null | undefined> | string | null | undefined;
+export function hxGet(callback: Callback): void;
