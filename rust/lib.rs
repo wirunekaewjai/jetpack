74296b30e11fb3832988cb255029dd56943ab6ca
Wirune Kaewjai <wirune.kae@hotmail.com>
2024-06-02 09:45:27 +0700

    update etag

--- rust/lib.rs
+++ rust/lib.rs
@@ -1,14 +1,14 @@
 pub mod html;
 
+mod bind_etag_header;
 mod create_etag;
-mod create_etag_response;
 mod create_hash;
 mod get_file_extension;
 mod get_file_mime;
 mod is_not_modified;
 
+pub use bind_etag_header::*;
 pub use create_etag::*;
-pub use create_etag_response::*;
 pub use create_hash::*;
 pub use get_file_extension::*;
 pub use get_file_mime::*;
